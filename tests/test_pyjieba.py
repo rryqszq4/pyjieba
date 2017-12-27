a85349d1a496fd910c76240376b755577037938e
quanzhao <279664262@163.com>
2017-12-27 17:49:53 +0800

    [t] The object pyjieba in jieba , add jieba.tag method and get word pos.

--- tests/test_pyjieba.py
+++ tests/test_pyjieba.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import pyjieba
 
-a = pyjieba.pyjieba(
+a = pyjieba.jieba(
         'cppjieba/dict/jieba.dict.utf8',
         'cppjieba/dict/hmm_model.utf8',
         'cppjieba/dict/user.dict.utf8',
@@ -11,14 +11,19 @@ a = pyjieba.pyjieba(
 
 print a
 
-b = a.cut("小明硕士毕业于中国科学院计算所，后在日本京都大学深造");
+b = a.cut("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")
 
-print b
+print "/".join(b)
 
-b = a.cut_all("小明硕士毕业于中国科学院计算所，后在日本京都大学深造");
+b = a.cut_all("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")
 
-print b
+print "/".join(b)
 
-b = a.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造");
+b = a.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")
 
-print b
+print "/".join(b)
+
+b = a.tag("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。")
+
+for x,y in b.iteritems():
+    print x,y
